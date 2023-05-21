1ceb73337e2ef3517c6b4f526d205508a728f4c4
KostiantynPetrochuk <firstpickpudge@gmail.com>
2023-05-21 21:55:35 +0300

    added hightlight for ordering inputs

--- src/partials/Ordering/OrderingInputText/OrderingInputText.tsx
+++ src/partials/Ordering/OrderingInputText/OrderingInputText.tsx
@@ -1,29 +1,58 @@
+import { useState } from "react";
+
 type OrderingInputProps = {
   inputName: string;
   inputClassName: string;
+  min?: number;
   value: string;
   setValue: React.Dispatch<React.SetStateAction<string>>;
 };
 
 const OrderingInputText = (props: OrderingInputProps): JSX.Element => {
-  const { inputName, inputClassName, value, setValue } = props;
+  const { inputName, inputClassName, min = 3, value, setValue } = props;
+  const [isInputValid, setIsInputValid] = useState<Boolean>(true);
   const labelClassName = `basket-ordering-${inputClassName}`;
 
-  const handleChangeValue = (event: React.ChangeEvent<HTMLInputElement>) =>
+  const handleChangeValue = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
+    if (event.target.value.length > min - 1 && !isInputValid) {
+      setIsInputValid(true);
+    }
+
+    if (event.target.value.length < min && isInputValid) {
+      setIsInputValid(false);
+    }
+  };
+
+  const handleInputBlur = () => {
+    if (value.length < min) {
+      setIsInputValid(false);
+      return;
+    }
+    setIsInputValid(true);
+  };
 
   return (
     <label className={labelClassName}>
       <span className="basket-ordering-input__name">{inputName}</span>
       <input
-        className="basket-ordering__input"
+        required
+        className={`basket-ordering__input${isInputValid ? "" : " _notValid"}`}
         type="text"
         name=""
         id=""
         value={value}
         onChange={handleChangeValue}
         placeholder={inputName}
+        onBlur={handleInputBlur}
       />
+      <span
+        className={`basket-ordering-input__message${
+          isInputValid ? "" : " _notValid"
+        }`}
+      >
+        Заповніть поле. Мінімум 3 символи
+      </span>
     </label>
   );
 };
