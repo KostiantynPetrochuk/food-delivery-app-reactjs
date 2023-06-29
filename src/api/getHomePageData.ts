fa6c8f610a8fe08c4b50dbafd2f8b215fd285e6d
KostiantynPetrochuk <firstpickpudge@gmail.com>
2023-06-29 20:40:30 +0300

    added logs

--- src/api/getHomePageData.ts
+++ src/api/getHomePageData.ts
@@ -2,8 +2,10 @@ import { API_URL } from "../env";
 
 const getHomePageData = async () => {
   const apiUrl: string = `${API_URL}`;
+  console.log("API_URL", API_URL);
 
   const queryString: string = `${apiUrl}/api/homePage/`;
+  console.log("queryString", queryString);
 
   const res: Response = await fetch(queryString, {
     method: "GET",
